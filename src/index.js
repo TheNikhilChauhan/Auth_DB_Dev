77aa42befd47b129f292b02dec01c7edd5ad5eba
Nikhil <ace.nikchauhan@gmail.com>
2024-07-29 16:20:21 +0530

    added roles and user_roles through table

--- src/index.js
+++ src/index.js
@@ -2,8 +2,7 @@ const express = require("express");
 const { PORT } = require("./config/serverConfig");
 const bodyParser = require("body-parser");
 const apiRoutes = require("./routes/index");
-const UserService = require("./services/user-service");
-const UserRepository = require("./repository/user-repository");
+
 const app = express();
 
 const prepareAndStartServer = () => {
@@ -13,18 +12,9 @@ const prepareAndStartServer = () => {
   app.use("/api", apiRoutes);
   app.listen(3001, async () => {
     console.log(`Server started on Port ${PORT}`);
-
-    // const userRepo = new UserRepository();
-
-    // const user = await userRepo.getByEmail("bing@gmail.com");
-    // console.log(user);
-    // const newToken = service.createToken({ email: "bing@gmail.com", id: "9" });
-    // console.log(newToken);
-
-    // const token =
-    //   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImJpbmdAZ21haWwuY29tIiwiaWQiOiI5IiwiaWF0IjoxNzIxOTg1NTA5LCJleHAiOjE3MjI1OTAzMDl9.pIpUwlLTrRTc89U7GYYf88wzTvQ-acdOZC42nkWmZNU";
-    // const response = service.verifyToken(token);
-    // console.log(response);
+    // if (process.env.DB_SYNC) {
+    //   db.sequelize.sync({ alter: true });
+    // }
   });
 };
 
